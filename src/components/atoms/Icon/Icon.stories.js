728f4e193efb0fc3789fcef3099cc4ecee2f6179
Josh Lane <joshuadlane85@gmail.com>
2023-08-06 23:07:51 -0400

    add test icon story

---
+++ src/components/atoms/Icon/Icon.stories.js
@@ -0,0 +1,83 @@
+import Button from '../index';
+import theme from '../Button.theme';
+import { dynamicValueDescription } from '@utils/storybookUtils';
+
+export default {
+  title: 'Components/Button/Danger',
+  component: Button,
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    outline: {
+      control: 'boolean',
+    },
+    theme: {
+      options: Object.keys(theme),
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+      description: dynamicValueDescription(
+        'Determines the visual output of the component.',
+        Object.keys(theme)
+      ),
+    },
+  },
+};
+
+const staticProps = {
+  onClick: () => {
+    alert('hey, nice click bro!');
+  },
+  children: 'My Button',
+  disabled: false,
+  outline: false,
+  theme: 'danger',
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = { ...staticProps };
+
+export const Disabled = Template.bind({});
+Disabled.args = { ...staticProps, disabled: true };
+
+export const FullWidth = Template.bind({});
+FullWidth.args = { ...staticProps, fullWidth: true };
+
+export const FullWidthDisabled = Template.bind({});
+FullWidthDisabled.args = { ...staticProps, fullWidth: true, disabled: true };
+
+export const Outline = Template.bind({});
+Outline.args = { ...staticProps, outline: true };
+Outline.story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+};
+
+export const FullWidthOutline = Template.bind({});
+FullWidthOutline.args = { ...staticProps, fullWidth: true, outline: true };
+FullWidthOutline.story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+};
+
+export const DisabledOutline = Template.bind({});
+DisabledOutline.args = { ...staticProps, disabled: true, outline: true };
+DisabledOutline.story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+};
+
+export const FullwidthDisabledOutline = Template.bind({});
+FullwidthDisabledOutline.args = { ...staticProps, disabled: true, outline: true, fullWidth: true };
+FullwidthDisabledOutline.story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+};
