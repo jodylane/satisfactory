922bdb124234714770038d4909e9a7284929e5c6
Josh Lane <joshuadlane85@gmail.com>
2023-08-13 21:35:34 -0400

    cleaning up styles, testing Icon on index page

--- src/components/molecules/ColorSwatches/ListSwatches.styled.js
+++ src/components/molecules/ColorSwatches/ListSwatches.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   gap: 20px;
-  grid-template: auto / repeat(auto-fill, minmax(150px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   max-width: 1000px;
   margin: 0 auto;
 `;
